9a77957350c9c7f279ee732b5aee2fc8b58f8b26
Shadowdragon8969 <shadowdragon8969@gmail.com>
2019-09-11 15:05:15 -0500

    Update ArrayList.h

--- GameLibs/ArrayList.h
+++ GameLibs/ArrayList.h
@@ -1,3 +1,51 @@
 #include <stdio.h>
 
+class ArrayList<object> {
+  private:
+  object** ptrs;
+  int size = 0;
+  public:
+  ArrayList() {
+    
+  }
+  void add(object* o) {
+    object** newlist = new object*[size + 1];
+    if (size == 0) {
+      ptrs[0] = o;
+      return;
+    }
+    for (int i = 0; i < size; i++) {
+      if (ptrs[i] == nullptr) {
+        continue;
+      }
+     newlist[i] = ptrs[i];
+    }
+    ptrs = newlist;
+    size++;
+    ptrs[size] = o;
+  }
+  void remove(int index) {
+   ptrs[index] = nullptr;
+  }
+  void remove(object* o) {
+    bool found = false;
+   for (int i = 0; i < size; i++) {
+     if (ptrs[i] == o) {
+       ptrs[i] = nullptr;
+       size--;
+       found = true;
+       break;
+     }
+   }
+    if (!found) return;
+    object** newlist = new object*[size];
+    for (int i = 0; i < size; i++) {
+      if (ptrs[i] == nullptr) {
+        continue;
+      }
+     newlist[i] = ptrs[i];
+    }
+  }
+  
+}
 
